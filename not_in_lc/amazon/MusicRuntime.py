a73665800179abee64a00343ed7162bd576ffa5c
leslierere <leslierenzy@gmail.com>
2021-07-14 12:31:32 -0500

    done for MusicRuntime.py

--- not_in_lc/amazon/MusicRuntime.py
+++ not_in_lc/amazon/MusicRuntime.py
@@ -1,28 +1,26 @@
 class Solution:
     def findMusic(self, rideDuration, songDuration):
         rideDuration -= 30
-        index_map = dict()
-        for i in range(len(songDuration)):
-            song = songDuration[i]
-            index_map[song] = i
+        max_len = -1
+        seen = dict()
+        res = [-1, -1]
+        for i, song in enumerate(songDuration):
+            if rideDuration-song in seen:
+                temp_max = max(rideDuration-song, song)
+                if temp_max > max_len:
+                    max_len = temp_max
+                    res = [seen[rideDuration-song], i]
+            seen[song] = i
 
-        songDuration.sort()
-        left = 0
-        right = len(songDuration) - 1
-
-        while left < right:
-            total_len = songDuration[left] + songDuration[right]
-            if total_len < rideDuration:
-                left += 1
-            elif total_len > rideDuration:
-                right -= 1
-            else:
-                return [index_map[songDuration[left]], index_map[songDuration[right]]]
-
-        return [-1, -1]
+        return res
 
 if __name__ == '__main__':
     solution = Solution()
     rideDuration = 90
     songDuration = [1, 10, 25, 35, 60]
     print(solution.findMusic(rideDuration, songDuration)) # should be [2, 3]
+    songDuration = [1, 10, 25, 35, 50]
+    print(solution.findMusic(rideDuration, songDuration))  # should be [1, 4]
+    rideDuration = 30
+    songDuration = [0, 0]
+    print(solution.findMusic(rideDuration, songDuration))  # should be [0, 1]
