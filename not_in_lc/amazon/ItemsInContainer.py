656ca6f714320b4b10cba8870beedd850e88d3e0
leslierere <leslierenzy@gmail.com>
2021-07-14 13:24:56 -0500

    done for itemsInContainer

--- not_in_lc/amazon/ItemsInContainer.py
+++ not_in_lc/amazon/ItemsInContainer.py
@@ -1,5 +1,6 @@
 class Solution:
     def noItems(self, s:str, startIndices, endIndices):
+        # from start(included) to end(included), how many items we have
         dp = [[0 for i in range(len(s))] for j in range(len(s))]
 
         first_idx = s.find("|")
@@ -11,13 +12,24 @@ class Solution:
 
         dp[first_idx][second_idx] = second_idx - first_idx - 1
 
+        left_bar = [-1 for i in range(len(s))]
+        # store the bar closest to the current index on the left side
+        last_bar = -1
+        for i in range(len(s)):
+            left_bar[i] = last_bar
+            if s[i] == "|":
+                last_bar = i
+
         for start in range(len(s)-2):
             for end in range(start+1, len(s)):
                 if s[end] == "|":
-                    for j in range(end-1, start-1, -1): # store before
-                        if s[j] == "|":
-                            dp[start][end] = dp[start][j] + end - j -1
-                            break
+                    pre_bar = left_bar[end]
+                    # for j in range(end-1, start-1, -1): # store before
+                    #     if s[j] == "|":
+                    #         dp[start][end] = dp[start][j] + end - j -1
+                    #         break
+                    if pre_bar != -1 and pre_bar >= start:
+                        dp[start][end] = dp[start][pre_bar] + end - pre_bar -1
                 else:
                     dp[start][end] = dp[start][end-1]
 
@@ -35,5 +47,13 @@ if __name__ == '__main__':
     startIndices = [1, 1]
     endIndices = [5, 6]
     print(solution.noItems(s, startIndices, endIndices)) # should be [2,3].
+    s = '*****|*'
+    startIndices = [1, 1]
+    endIndices = [5, 6]
+    print(solution.noItems(s, startIndices, endIndices))  # should be [0, 0].
+    s = '**|**|*|*|'
+    startIndices = [1, 1]
+    endIndices = [5, 6]
+    print(solution.noItems(s, startIndices, endIndices))  # should be
 
 
